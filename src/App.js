8cf1344d8938c040b4b9d64e1f80d59f278e2901
jeffren <jeffrenmokamo726@gmail.com>
2024-09-23 21:45:51 +0200

    preload

--- src/App.js
+++ src/App.js
@@ -1,23 +1,35 @@
-import logo from './logo.svg';
-import './App.css';
+import './css/App.css'
+import Preload from './composants/Preload'
+import React, {  useEffect, useState} from 'react'
 
-function App() {
+function App(){
+
+  const [loading, setLoading] = useState(true)
+
+  useEffect(() => {
+
+    setTimeout(() => {
+
+      setLoading(false)
+    }, 3000);
+
+  }, [])
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      
+      { loading ? (<Preload />) : 
+      
+      (
+
+        <div>
+
+          <h1>Jeffren Mokamo</h1>
+          
+        </div>
+
+      )}
+
     </div>
   );
 }
---
+++ src/composants/Preload.js
@@ -0,0 +1,14 @@
+import logo from '../assets/logos/logo.png'
+
+function Preload() {
+
+    return (
+      <div className="preload">
+                
+            <img src={logo} alt="logo" />
+        
+       </div>
+    );
+}
+ 
+export default Preload;
